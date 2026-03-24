9183bd559b0d2a092ba034e8c47f09ad06f26200
Danil <chyksin@bk.ru>
2026-03-24 12:45:08 +0700

    1

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	npm ci
+git: 
+	git add . && git commit -m "1" && git push
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const _ = require('lodash');
-console.log("Helloy, Hexlet!")
-console.log(_.last([2, 3, 4, 5]))
+import _ from 'lodash'
+// const _ = require("lodash");
+console.log("Helloy, Hexlet!");
+console.log(_.last([2, 3, 4, 5]));
